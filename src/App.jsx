d5833e8db2f9f1211c7218f8f0892e1646ae17f3
Fadhel <fadhel.s.hashem@gmail.com>
2026-07-27 16:19:34 +0300

    basic looping

--- src/App.jsx
+++ src/App.jsx
@@ -4,56 +4,73 @@ import './App.css'
 const App = () => {
 
   const initialState = [
-  {
-    id: 1,
-    name: "Black T shirt",
-    price: 5
-  },
-  {
-    id: 2,
-    name: "Hanger Set",
-    price: 8
-  },
-  {
-    id: 3,
-    name: "Thoub",
-    price: 12
-  },
-  {
-    id: 4,
-    name: "Computer Bag",
-    price: 20
-  },
-  {
-    id: 5,
-    name: "Couch",
-    price: 50
-  },
-  {
-    id: 6,
-    name: "Gaming Chair",
-    price: 70
-  }
-]
+    {
+      id: 1,
+      name: "Black T shirt",
+      price: 5
+    },
+    {
+      id: 2,
+      name: "Hanger Set",
+      price: 8
+    },
+    {
+      id: 3,
+      name: "Thoub",
+      price: 12
+    },
+    {
+      id: 4,
+      name: "Computer Bag",
+      price: 20
+    },
+    {
+      id: 5,
+      name: "Couch",
+      price: 50
+    },
+    {
+      id: 6,
+      name: "Gaming Chair",
+      price: 70
+    }
+  ]
 
-const shoppingCart = []
-const availableBalance = 100
+  const availableBalance = 100
+  const [availableItems, setAvailableItems] = useState(initialState)
+  const [shoppingCart, setShoppingCart] = useState([])
 
-const [availableItems , setAvailableItems ] = useState(initialState)
+  const handleAddToCart = () => {}
 
 
   return (
 
     <div>
 
-    <h1>Zaids Closet</h1>
-<h2>Your Balance: {availableBalance} </h2>
-<h2>Available Items</h2>
-<h2>Shopping Cart</h2>
+      <h1>Zaids Closet</h1>
+      <h2>Your Balance: {availableBalance} </h2>
+      <h2>Available Items</h2>
+      <br />
+      {availableItems.map((availableItem) => (
+        <>
+          <p>{availableItem.name} : {availableItem.price}</p>
 
- </div>
+          <h2>Shopping Cart</h2>
+          {shoppingCart.map((cartItem) => (
+            
+              <p>{cartItem}</p>
+            
+          ))}
+          
+          <button>Add to Cart</button>
+
+        </>
+      ))}
+      
+    </div>
 
   )
 }
+
 
 export default App
